431f14ef6e6d5b783baaac57048f99cd00a69b2a
Oksana-Lukova <oksana_lukova@external.mckinsey.com>
2019-06-18 11:14:22 +0300

    add index

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
+
+const TOKEN = process.env.TELEGRAM_API_TOKEN
+
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+
+bot.on('message', msg => {
+    const {chat: {id}} = msg
+    bot.sendMessage(id, "Hello")
+})
